564c057a3c93e5cdd62e7b23464472f76e1579ae
chizk0 <126604899+chizk0@users.noreply.github.com>
2023-09-18 22:30:10 -0600

    [IMPLEMENT BURGER BUTTON]

--- ims_app/root/components/dropdown-button/DropDownButton.tsx
+++ ims_app/root/components/dropdown-button/DropDownButton.tsx
@@ -3,17 +3,24 @@ import React, { useState } from 'react';
 const DropdownButton = () => {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
-  const toggleDropdown = () => {
-    setIsDropdownOpen(prevState => !prevState);
-  };
+  const menuRef = React.useRef<HTMLDivElement>(null);
+  const buttonRef = React.useRef<HTMLButtonElement>(null);
 
+  window.addEventListener('click', (e) => {
+    if (e.target !== buttonRef.current && e.target !== menuRef.current) {
+
+      setIsDropdownOpen(false);
+
+    }
+  });
   return (
     <div className="relative inline-block">
       <button
+        ref={buttonRef}
         id="dropdownDefaultButton"
         className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         type="button"
-        onClick={toggleDropdown}
+        onClick={() => setIsDropdownOpen(!isDropdownOpen)}
       >
         Geizel Valverde
         <svg
@@ -34,6 +41,7 @@ const DropdownButton = () => {
       </button>
       {isDropdownOpen && (
         <div
+          ref={menuRef}
           id="dropdown"
           className="z-10 absolute bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700"
           aria-labelledby="dropdownDefaultButton"
