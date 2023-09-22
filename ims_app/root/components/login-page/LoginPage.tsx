a375e0218d9cbc187b6cde51c7e2c58e59ffaf3f
JesusAbarcaRodriguez <jesus.abarca.rodriguez@est.una.ac.cr>
2023-09-22 13:01:34 -0600

    Update LoginPage.tsx

--- ims_app/root/components/login-page/LoginPage.tsx
+++ ims_app/root/components/login-page/LoginPage.tsx
@@ -31,18 +31,6 @@ export default function LoginPage() {
                             <div className="flex flex-col justify-start items-center">
                                 <h1 className="w-full font-bold mb-4 text-center border-2 border-slate-950 dark:border-gray-600 rounded-lg p-4 m-4">BIENVENIDO</h1>
                             </div>
-                            <Form className="flex flex-col  w-full p-4">
-                                <CustomInput label="Email:" name="email" inputType="email"  />
-                                <CustomInput label="Password:" inputType="password" name="password"  />
-                                <div className="flex justify-center w-full">
-                                    <button type="submit" className="mt-4 bg-blue-700 hover:bg-blue-500 rounded-md p-2 w-full flex items-center justify-center text-white">
-                                        <span className="mr-2">Log In</span>
-                                    </button>
-                                </div>
-                            </Form>
-                            <Link href="/private/reset" className="flex flex-col text-center mt-2 hover:text-blue-600 hover:underline">
-                                Forgot my password, <span className="text-blue-400">click here</span>
-                            </Link>
                         </div>
                     </div>
                 </div>
