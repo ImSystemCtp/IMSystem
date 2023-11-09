c62238447c353759418682f4da8f16a854c2f8d4
chizk0 <126604899+chizk0@users.noreply.github.com>
2023-11-09 11:24:42 -0600

    [UPDATE ] Post assets

--- ims_app/app/api/register/assets/route.ts
+++ ims_app/app/api/register/assets/route.ts
@@ -1,8 +1,6 @@
 import { prismaDB } from "@/lib";
 import { NextResponse } from "next/server";
-import { ims_assets, ims_registered_in } from "@prisma/client";
 import { registerGood } from "@/root/types";
-import { getNextNumber } from "../../(function)";
 export async function POST(req: Request) {
     try {
         const body = await req.json() as registerGood;
@@ -10,26 +8,27 @@ export async function POST(req: Request) {
         const assets = body.assets;
         if (type == 'Register') {
 
-            assets.forEach(async (element: ims_assets) => {
-                //const registerId = response.reg_id;
-
-                const [reg] = await prismaDB.$transaction([
-                    prismaDB.ims_register.create({
+                for (const element of assets) {
+                    console.log('1')
+                    setTimeout(function () {
+                        console.log('2');
+                    }, 2000)
+                    const reg = await prismaDB.ims_register.create({
                         data: {
-                            reg_inst_id: 1,
-                            reg_folio: 1,
-                            reg_tomo:  1,
-                            reg_asiento: 1,
-                            reg_type: body.register.reg_type,
-                            reg_observation: body.register.reg_observation,
-                            reg_usu_id: body.register.reg_usu_id,
-                        }
-                    }),
-                    prismaDB.$executeRaw`call increment_register_in()`
-                ]);
-                prismaDB.ims_assets.create({ data: element })
-                prismaDB.ims_register_assets.create({ data: { reg_id: reg.reg_id, assets_no: element.assets_no } })
-            });
+                        reg_inst_id: 1,
+                        reg_folio: 1,
+                        reg_tomo: 1,
+                        reg_asiento: 1,
+                        reg_type: body.register.reg_type,
+                        reg_observation: body.register.reg_observation,
+                        reg_usu_id: body.register.reg_usu_id,
+                        },
+                    });
+                    console.log(reg.reg_asiento);
+                    await prismaDB.ims_assets.create({ data: element });
+                    await prismaDB.ims_register_assets.create({ data: { reg_id: reg.reg_id, assets_no: element.assets_no } });
+                }
+                
             return NextResponse.json({ message: "Register" });
         }
         return new NextResponse("type no .... ", { status: 401 });
