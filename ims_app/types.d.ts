6db2b9772b44a9c771733044184bd27d908e6cc8
chizk0 <126604899+chizk0@users.noreply.github.com>
2023-11-12 12:03:32 -0600

    [ADD] registerToReport

--- ims_app/types.d.ts
+++ ims_app/types.d.ts
@@ -22,3 +22,24 @@ interface QueryParams {
         | "notIn";
     }
 }
+interface registerToReport {
+    reg_tomo: number
+    reg_folio: number
+    reg_asiento: number
+    reg_type: string
+    reg_date: string
+    reg_observation: string
+    invoice_date: string | null;
+    assets_no: string;
+    assets_description: string;
+    assets_brand: string;
+    assets_model: string;
+    assets_series: string;
+    assets_state: $Enums.EnumAssetsState;
+    assets_acquisition_value: string;
+    assets_invoice_number: string | null;
+    location_name: string;
+    law_name: string;
+    usu_name: string;
+
+}
