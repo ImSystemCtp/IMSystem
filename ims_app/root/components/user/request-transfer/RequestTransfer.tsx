58132aa4ec18496ca707d209d6fc9d41e45efec9
JesusAbarcaRodriguez <jesus.abarca.rodriguez@est.una.ac.cr>
2023-11-25 14:32:46 -0600

    [IMPLEMENT GLOBAL RESPONSIVE IN USER MODULES]
    Igual que en Admin, se implemento el responsive desing, solo falta mejorarlo en algunas partes.

--- ims_app/root/components/user/request-transfer/RequestTransfer.tsx
+++ ims_app/root/components/user/request-transfer/RequestTransfer.tsx
@@ -6,13 +6,13 @@ export default function RequestTransfer() {
     useAuth();
     return (
         <div className="w-full">
-            <h2 className="text-2xl font-bold  text-center">Registrar Solicitud de Traslados</h2>
+            <h2 className="m-2 text-2xl font-bold  text-center">Registrar Solicitud de Traslados</h2>
             <SearchAssets />
             <div className=" flex flex-col lg:flex-row lg:m-2 lg:p-2">
                 <div className=" w-full lg:w-3/5 pr-0 lg:pr-4">
                     <AssetsUserTable />
                 </div>
-                <div className="border-2  rounded-lg border-slate-300 shadow-sm shadow-slate-300  w-full lg:w-2/5">
+                <div className="m-2 border-2  rounded-lg border-slate-300 shadow-sm shadow-slate-300  w-full lg:w-2/5">
                     <RequestTransferForm />
                 </div>
             </div>
--- ims_app/root/components/user/user-navbar/UserNavBar.tsx
+++ ims_app/root/components/user/user-navbar/UserNavBar.tsx
@@ -9,6 +9,12 @@ export default function UserNavBar() {
     const handleClick = () => {
         router.push("/user");
     }
+    const toggleMenu = () => {
+        const navbarDefault = document.getElementById("navbar-default");
+        if (navbarDefault) {
+            navbarDefault.classList.toggle("hidden");
+        }
+    };
     return (
         <nav className="bg-sky-400 border-gray-200 dark:bg-gray-900">
             <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
@@ -17,6 +23,12 @@ export default function UserNavBar() {
                     <motion.button onClick={handleClick} whileHover={{ scale: 1.05 }}
                         whileTap={{ scale: 0.95 }} className="text-2xl font-semibold whitespace-nowrap dark:text-white">IM_System</motion.button>
                 </div>
+                <button onClick={toggleMenu} data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
+                    <span className="sr-only">Open main menu</span>
+                    <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15" />
+                    </svg>
+                </button>
                 <div className="hidden w-full md:block md:w-auto" id="navbar-default">
                     <ul className="bg-sky-400 font-medium flex flex-col p-4 md:p-0 mt-4 border  rounded-lg  md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0  dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                         <li>
