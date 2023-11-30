676f286724cd4dfc1b0df2f03cac61661cfff504
JesusAbarcaRodriguez <jesus.abarca.rodriguez@est.una.ac.cr>
2023-11-29 18:56:45 -0600

    [UPDATE Schemas]

--- ims_app/schemas/login-form-message.ts
+++
@@ -1,5 +0,0 @@
-import * as Yup from "yup";
-const loginMessage = Yup.object({
-    email: Yup.string().email("Invalid email format").required("Email is required"),
-});
-export default loginMessage;
--- ims_app/schemas/user-edit-message.ts
+++
@@ -1,6 +0,0 @@
-import * as Yup from "yup";
-const UserEditMessage = Yup.object().shape({
-    name: Yup.string().required("The name is required").min(2, "The name must be at least 2 characters long"),
-    contact: Yup.string().required("This field is required"),
-});
-export default UserEditMessage;
