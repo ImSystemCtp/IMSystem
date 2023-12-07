6fa5de226392eba910db803e8392105d43ff8085
Francisco Amador <126604899+chizk0@users.noreply.github.com>
2023-12-06 21:51:58 -0600

    Refactor PrismaClient import and initialization

--- ims_app/lib/prisma/prismadb.ts
+++ ims_app/lib/prisma/prismadb.ts
@@ -1,16 +1,28 @@
-/* import { PrismaClient } from "@prisma/client";
-
-const prismaDB = new PrismaClient();
-
-export default prismaDB; */
-
 import { PrismaClient } from "@prisma/client"
 
-declare global {
-    var prisma: PrismaClient | undefined
-}
 
-const prismaDB = globalThis.prisma || new PrismaClient()
-if (process.env.NODE_ENV !== "production") globalThis.prisma = prismaDB
+// PrismaClient is attached to the `global` object in development to prevent
+// exhausting your database connection limit.
+//
+// Learn more:
+// https://pris.ly/d/help/next-js-best-practices
 
-export default prismaDB;
+const globalForPrisma = global as unknown as { prisma: PrismaClient }
+
+export const prismaDB = globalForPrisma.prisma || new PrismaClient()
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prismaDB
+
+export default prismaDB
+
+
+// import { PrismaClient } from "@prisma/client"
+
+// declare global {
+//     var prisma: PrismaClient | undefined
+// }
+
+// const prismaDB = globalThis.prisma || new PrismaClient()
+// if (process.env.NODE_ENV !== "production") globalThis.prisma = prismaDB
+
+// export default prismaDB;
