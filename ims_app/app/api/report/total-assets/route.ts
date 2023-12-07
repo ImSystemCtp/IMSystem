c0afad4850bb27717f1bfdd6c1e577d56d12b376
Francisco Amador <126604899+chizk0@users.noreply.github.com>
2023-12-06 22:19:50 -0600

    Refactor total assets route to return unique entries

--- ims_app/app/api/report/total-assets/route.ts
+++ ims_app/app/api/report/total-assets/route.ts
@@ -10,9 +10,25 @@ export async function GET(req: Request) {
         JOin ims_register r on r.reg_id=ra.reg_id
         JOIN ims_locations l on l.location_id=a.assets_curr_location
         JOIN ims_laws la on la.law_id = a.assent_law_id
-        JOIN ims_users u on u.usu_id = r.reg_usu_id`;
-        return NextResponse.json(registers);
+        JOIN ims_users u on u.usu_id = r.reg_usu_id
+        ORDER by r.reg_tomo, r.reg_folio, r.reg_asiento`;
+        const registrosUnicos = filterUniqueEntries(registers);
+
+        return NextResponse.json(registrosUnicos);
     } catch (error) {
         return new NextResponse("Error interno del servidor", { status: 500 });
     }
 }
+function filterUniqueEntries(arr: registerToReport[]): registerToReport[] {
+    const uniqueEntries = arr.reduce((unique: Map<string, registerToReport>, entry) => {
+        const key = `${entry.reg_folio}-${entry.reg_tomo}-${entry.reg_asiento}`;
+        if (!unique.has(key)) {
+            unique.set(key, entry);
+        }
+        return unique;
+    }, new Map<string, registerToReport>());
+
+    return Array.from(uniqueEntries.values());
+}
+
+
