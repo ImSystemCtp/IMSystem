9e3b8c7f783cd5970d10dda41f5d3847faf148e9
Francisco Amador <126604899+chizk0@users.noreply.github.com>
2023-12-08 16:52:55 -0600

    Refactor asset search route to improve code readability and maintainability

--- ims_app/app/api/assets/search/route.ts
+++ ims_app/app/api/assets/search/route.ts
@@ -6,74 +6,43 @@ export async function GET(req: Request) {
     try {
         const url = req.url;
         const { searchParams } = new URL(url)
-        let Asset = searchParams.get('asset')
-        let location = searchParams.get('location')
-        let response;
-        console.log(Asset)
-        console.log(location)
-        if (Asset === null) {
-            Asset = ''; // or any default value
-        }
-        if (location === null) {
-            location = ''; // or any default value
-        }
-        if (location !== '') {
-            response = await prismaDB.ims_assets.findMany({
-                where: {
-                    AND: [
-                        {
-                            OR: [{
-                                assets_no: { contains: Asset }
-                            }, {
-                                assets_description: { contains: Asset }
-                            }, {
-                                assets_model: { contains: Asset }
-                            }, {
-                                assets_series: { contains: Asset }
-                            }, {
-                                assets_brand: { contains: Asset }
-                            }]
-                        },
-                        {
-                            assets_curr_location: {
-                                equals: parseInt(location)
-                            }
-                        }, {
-                            assets_state: {
-                                not: $Enums.EnumAssetsState.Malo
-                            }
-                        }]
-                },
-                take:10
-            })
-        } else {
-            response = await prismaDB.ims_assets.findMany({
-                where: {
-                    AND: [
-                        {
-                            OR: [{
-                                assets_no: { contains: Asset }
-                            }, {
-                                assets_description: { contains: Asset }
-                            }, {
-                                assets_model: { contains: Asset }
-                            }, {
-                                assets_series: { contains: Asset }
-                            }, {
-                                assets_brand: { contains: Asset }
-                            }]
-                        },
-                        {
-                            assets_state: {
-                                not: $Enums.EnumAssetsState.Malo
-                            }
-                        }]
-                    
-                },
-                take:10
-            })
-        }
+        const Asset = searchParams.get('asset')|| ""
+        const location = searchParams.get('location')|| ""
 
+        const assetQuery:any = {
+            where: {
+                AND: [
+                    {
+                        OR: [
+                            { assets_no: { contains: Asset } },
+                            { assets_description: { contains: Asset } },
+                            { assets_model: { contains: Asset } },
+                            { assets_series: { contains: Asset } },
+                            { assets_brand: { contains: Asset } }
+                        ]
+                    },
+                    {
+                        assets_state: {
+                            not: $Enums.EnumAssetsState.Malo
+                        }
+                    }
+                ]
+            },
+            take: 10
+        };
+    
+        if (location !== '') {
+            assetQuery.where.AND.push({
+                assets_curr_location: {
+                    equals: parseInt(location)
+                }
+            });
+        }
+        if (Asset === '') {
+            assetQuery.take=100;
+        }
+        console.log(assetQuery)
+        const response = await prismaDB.ims_assets.findMany(assetQuery);
         return Response.json(response);
     } catch (error) {
         console.log(error)
