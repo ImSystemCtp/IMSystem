4755e2e439da00df45c4308b6d6ba10148956d82
JesusAbarcaRodriguez <jesus.abarca.rodriguez@est.una.ac.cr>
2024-01-11 10:54:56 -0600

    [ADD Info in About.tsx & Contact.tsx]

--- ims_app/root/components/footer/AboutModal.tsx
+++ ims_app/root/components/footer/AboutModal.tsx
@@ -4,10 +4,10 @@ type ModalProps = {
     isOpen: boolean;
     onRequestClose: () => void;
 };
-export default function AboutModal ({
+export default function AboutModal({
     isOpen,
     onRequestClose,
-}: ModalProps){
+}: ModalProps) {
     return (
         <Modal
             isOpen={isOpen}
@@ -15,10 +15,11 @@ export default function AboutModal ({
             contentLabel="Seleccionar Rol"
             className="modal-content bg-white rounded-md p-8 max-w-90vw max-h-90vh relative"
             overlayClassName="modal-overlay fixed inset-0 flex items-center justify-center bg-black bg-opacity-50"
-        > <button
-            onClick={onRequestClose}
-            className="absolute top-2 right-2 text-gray-500 hover:text-gray-700 focus:outline-none"
         >
+            <button
+                onClick={onRequestClose}
+                className="absolute top-2 right-2 text-gray-500 hover:text-gray-700 focus:outline-none"
+            >
                 <svg
                     className="w-6 h-6"
                     fill="none"
@@ -33,7 +34,24 @@ export default function AboutModal ({
                     />
                 </svg>
             </button>
-            <h2>Acerca De...</h2>
+            <h2 className="text-3xl font-bold text-center mb-4">Acerca De...</h2>
+            <section className="mb-6">
+                <h3 className="text-xl font-semibold mb-2">Descripción del Sistema</h3>
+                <p>
+                    Proporcionamos al Colegio Técnico Profesional de Platanares una
+                    herramienta eficiente para gestionar activos, registrar traslados, bajas y
+                    solicitar bajas, traslados de manera rápida y precisa.
+                </p>
+            </section>
+            <section className="mb-6">
+                <h3 className="text-xl font-semibold mb-2">Desarrolladores</h3>
+                <p>Jesus Abarca Rodríguez - Francisco Amador Salazar</p>
+            </section>
+            <section className="mb-6">
+                <h3 className="text-xl font-semibold mb-2">Líder Técnico</h3>
+                <p>Geizel Valverde Navarro, Coordinadora Técnica CTP Platanares</p>
+            </section>
         </Modal>
+
     )
 }
--- ims_app/root/components/footer/ContactModal.tsx
+++ ims_app/root/components/footer/ContactModal.tsx
@@ -4,10 +4,10 @@ type ModalProps = {
     isOpen: boolean;
     onRequestClose: () => void;
 };
-export default function ContactModal ({
+export default function ContactModal({
     isOpen,
     onRequestClose,
-}: ModalProps){
+}: ModalProps) {
     return (
         <Modal
             isOpen={isOpen}
@@ -15,10 +15,11 @@ export default function ContactModal ({
             contentLabel="Seleccionar Rol"
             className="modal-content bg-white rounded-md p-8 max-w-90vw max-h-90vh relative"
             overlayClassName="modal-overlay fixed inset-0 flex items-center justify-center bg-black bg-opacity-50"
-        > <button
-            onClick={onRequestClose}
-            className="absolute top-2 right-2 text-gray-500 hover:text-gray-700 focus:outline-none"
         >
+            <button
+                onClick={onRequestClose}
+                className="absolute top-2 right-2 text-gray-500 hover:text-gray-700 focus:outline-none"
+            >
                 <svg
                     className="w-6 h-6"
                     fill="none"
@@ -33,7 +34,33 @@ export default function ContactModal ({
                     />
                 </svg>
             </button>
-            <h2>Contacto:</h2>
+
+            <h2 className='text-3xl font-bold text-center'>Contacto:</h2>
+
+            <div className="mt-4">
+                <p>
+                    <strong>Correo:</strong> ctp.platanares@mep.go.cr
+                </p>
+                <p>
+                    <strong>Teléfono:</strong> 2737 0025
+                </p>
+                <p>
+                    <strong>Página web:</strong>{" "}
+                    <a
+                        href="https://ctpplatanares.ed.cr/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="text-blue-500 hover:underline"
+                    >
+                        https://ctpplatanares.ed.cr/
+                    </a>
+                </p>
+            </div>
+
+            <p className="mt-4">
+                <strong>Ubicación:</strong> Contiguo a la Iglesia Católica de San Rafael de Platanares, Pérez Zeledon, San José.
+            </p>
         </Modal>
-    )
+    );
+
 }
