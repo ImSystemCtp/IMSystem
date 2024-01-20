6b01dbb6d4552d4e3f1a17d50f59e11c14bbe1df
chizk0 <126604899+chizk0@users.noreply.github.com>
2024-01-19 19:25:19 -0600

    [UPDATE Endpoint register asset]
    Ahora actualiza el numero de placa si ya ha cambiado

--- ims_app/app/api/register/assets/route.ts
+++ ims_app/app/api/register/assets/route.ts
@@ -2,18 +2,24 @@ import prisma from "@/lib/prisma";
 import { registerAsset } from "@/lib/definitions";
 import { NextResponse } from "next/server";
 import { getNextNumber, getNumRegister, updateRegisterNumber } from "../../(function)";
+import { ims_assets } from "@prisma/client";
 export async function POST(req: Request) {
     try {
         const body = await req.json() as registerAsset;
         const type = body.register.reg_type;
-        const assets = body.assets;
-        const register_num = body.plate_num
+        let assets: ims_assets[] = body.assets;
+        const register_num = body.plate_num || 0
+        const dbNumRegister = await prisma.ims_institution.findMany().then((res) => res[0].inst_current_no_plate)
+        console.log(register_num == dbNumRegister)
+        if (register_num != dbNumRegister) {
+            assets = await updatePlateNumber(assets, dbNumRegister)
+        }
         let currentRegisterin = await getNumRegister()
         if (type == 'Register') {
-                for (const element of assets) {
+            for (const element of assets) {
 
-                    const reg = await prisma.ims_register.create({
-                        data: {
+                const reg = await prisma.ims_register.create({
+                    data: {
                         reg_inst_id: 1,
                         reg_folio: currentRegisterin.folio,
                         reg_tomo: currentRegisterin.tomo,
@@ -22,17 +28,34 @@ export async function POST(req: Request) {
                         reg_observation: body.register.reg_observation,
                         reg_usu_id: body.register.reg_usu_id,
                         reg_date: body.register.reg_date,
-                        },
-                    });
-                    await prisma.ims_assets.create({ data:{...element, assets_curr_location:element.assets_regis_location} });
-                    await prisma.ims_register_assets.create({ data: { reg_id: reg.reg_id, assets_no: element.assets_no } });
-                    currentRegisterin =await  getNextNumber(currentRegisterin)
-                }
-                updateRegisterNumber(currentRegisterin.tomo, currentRegisterin.folio, currentRegisterin.asiento)
+                    },
+                });
+                await prisma.ims_assets.create({ data: { ...element, assets_curr_location: element.assets_regis_location } });
+                await prisma.ims_register_assets.create({ data: { reg_id: reg.reg_id, assets_no: element.assets_no } });
+                currentRegisterin = await getNextNumber(currentRegisterin)
+            }
+            updateRegisterNumber(currentRegisterin.tomo, currentRegisterin.folio, currentRegisterin.asiento)
             return NextResponse.json({ message: "Register" });
         }
         return new NextResponse("type no .... ", { status: 401 });
     } catch (error) {
         return new NextResponse("Unauthorized", { status: 401 });
     }
+}
+
+const updatePlateNumber = async (assets: ims_assets[], plate_num: number) => {
+
+
+    const newAssets = assets.map((asset, index) => ({
+        ...asset,
+        assets_no: formatPlateNumber(Number(plate_num) + index + 1)
+    }));
+    return newAssets as ims_assets[];
+}
+
+
+export const formatPlateNumber = (number: Number) => {
+    const plateNumber = String(number);
+    const formattedNumber = plateNumber.padStart(4, '0');
+    return `4167-${formattedNumber}`;
 }
